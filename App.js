440f4c669f52a6178d858bcc1cafd871c087c103
cckn <cckn@cckn-MacBookPro.local>
2019-05-03 13:35:37 +0900

    Main page & input card

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Dimensions,
+  Platform
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>DS TODO</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder="TODO" />
+        </View>
       </View>
     );
   }
@@ -14,8 +28,37 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f23657",
+    alignItems: "center",
+    justifyContent: "flex-start"
   },
+  title: {
+    marginTop: 100,
+    fontSize: 40,
+    color: "white",
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 30,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
 });
