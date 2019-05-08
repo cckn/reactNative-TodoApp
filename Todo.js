ba869401554862096f741abf3abb71113bc464d9
cckn <cckn@cckn-MacBookPro.local>
2019-05-08 23:49:06 +0900

    Add icons

--- Todo.js
+++ Todo.js
@@ -14,27 +14,50 @@ class Todo extends Component {
     isCompleted: false
   };
   render() {
-    const { isCompleted } = this.state;
+    const { isCompleted, isEditing } = this.state;
     return (
       <View style={styles.container}>
-      <View></View>
-        <TouchableOpacity onPress={this._toggleComplete}>
-          <View
+        <View style={styles.column}>
+          <TouchableOpacity onPress={this._toggleComplete}>
+            <View
+              style={[
+                styles.circle,
+                isCompleted ? styles.completedCircle : styles.uncompletedCircle
+              ]}
+            />
+          </TouchableOpacity>
+          <Text
             style={[
-              styles.circle,
-              isCompleted ? styles.completedCircle : styles.uncompletedCircle
+              styles.text,
+              isCompleted ? styles.completedText : styles.uncompletedText
             ]}
-          />
-        </TouchableOpacity>
-        <Text
-          style={[
-            styles.text,
-            isCompleted ? styles.completedText : styles.uncompletedText
-          ]}
-        >
-          {" "}
-          Hello i'm Todo Component
-        </Text>
+          >
+            {" "}
+            Hello i'm Todo Component
+          </Text>
+        </View>
+        {isEditing ? (
+          <View style={styles.actions}>
+            <TouchableOpacity onPressOut={this._finishEditing}>
+              <View style={styles.actionContatiner}>
+                <Text style={styles.actionText}>✅</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.actions}>
+            <TouchableOpacity onPressOut={this._startEditing}>
+              <View style={styles.actionContatiner}>
+                <Text style={styles.actionText}>✏️</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={styles.actionContatiner}>
+                <Text style={styles.actionText}>❌</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     );
   }
@@ -46,6 +69,18 @@ class Todo extends Component {
       };
     });
   };
+
+  _startEditing = () => {
+    this.setState({
+      isEditing: true
+    });
+  };
+
+  _finishEditing = () => {
+    this.setState({
+      isEditing: false
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -54,7 +89,8 @@ const styles = StyleSheet.create({
     borderBottomColor: "#bbb",
     borderBottomWidth: StyleSheet.hairlineWidth,
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   circle: {
     width: 30,
@@ -70,13 +106,28 @@ const styles = StyleSheet.create({
     borderColor: "red"
   },
   text: { fontWeight: "600", fontSize: 20, marginVertical: 20 },
-  completedText:{
-    color:"#bbb",
-    textDecorationLine:"line-through"
+  completedText: {
+    color: "#bbb",
+    textDecorationLine: "line-through"
   },
-  uncompletedText:{
-color:"#353839"
-  }
+  uncompletedText: {
+    color: "#353839"
+  },
+
+  column: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: width / 2
+  },
+  actions: { flexDirection: "row" },
+  actionContatiner: {
+    marginVertical: 10,
+    marginHorizontal: 10
+    // borderColor: "black",
+    // borderWidth: 1
+  },
+  actionText: {}
 });
 
 export default Todo;
